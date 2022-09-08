6b8629cf6cfbeb7c8aede99d3fb0869f95a02b1c
Uduaka Colette Chioma <Opriscy42@gmail.com>
2022-09-08 05:59:44 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+1 #include <stdio.h>
+2
+3 /**
+4 * main - Entry point
+5 *
+6 * Return: Always 0 (Success)
+7 */
+8 int main(void)
+9 {
+10        puts( "\"Programming is like building
+11        return (0) ;
+12   }
