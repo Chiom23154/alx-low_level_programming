f93534f535cd56bcd49cedb0fe3884d7b3545d91
Uduaka Colette Chioma <Opriscy42@gmail.com>
2022-09-09 16:34:03 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,8 +14,8 @@ int main(void)
 	int d;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	d = n % 10;
+	n = rand() - RAND_MAX / 2 ;
+	d = n % 10 ;
 	if (d == 0)
 	{
 		printf("Last digitof %i is %i and is 0\n", n,d);
---
+++ README.md
@@ -0,0 +1,1 @@
+This project aims to learn about arithmetic opertaors, relational operators, boolean operators, comments, declaring variables and loops in C language
