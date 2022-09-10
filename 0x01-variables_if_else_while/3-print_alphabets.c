fde2166e105e3c730504db686f582000714d4ae6
Uduaka Colette Chioma <Opriscy42@gmail.com>
2022-09-10 03:30:23 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,18 @@
 #include<stdio.h>
 
 /**
- * main - prints alphabets
+ * main - Prints the alphabet.
  *
- * Description: Long description
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
-	char CH;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-	for (ch = 'a'; ch <= 'z'; ch++
-        {
-	       putchar(ch);
-	}
-
-	for (CH = 'A'; CH <= 'Z'; CH++
+	for (i = 0; i < 52; i++)
 	{
-	        putchar(CH);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
