162f71adf4ad10c0293cf321c1a82d7e4d4d2f85
Uduaka Colette Chioma <Opriscy42@gmail.com>
2022-10-06 14:16:50 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,7 @@
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
  *
- * Return: pointer to the resulting string
+ * Return: pointer to thee resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -20,21 +20,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 
 	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
+		s = malloc(sizeof(char) * (len + n + 1));
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-        if (!s)
-                return (NULL);
+	if (!s)
+		return (NULL);
 
-        while (i < len1)
-        {
-	        s[i] = s1[i];
-	        i++;
-        }
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
 
-        while (n < len2 && i < (len1 + n))
-	        s[i++] = s2[j++];
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
 	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
