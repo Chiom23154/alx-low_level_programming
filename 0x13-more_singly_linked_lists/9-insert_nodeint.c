39cde4d0f19acd45eb10252ab904b1b43606b7f3
Uduaka Colette Chioma <Opriscy42@gmail.com>
2022-10-26 06:03:23 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,7 +6,7 @@
  * @head: head of a list.
  * @idx: index of the list where the new node is
  * added.
- * @n: integer element.
+ * @n:; integer element.
  *
  * Return: the address of the new node, or NULL if it
  * failed.
@@ -32,9 +32,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-		return (NULL);
+		return (null);
 
-	new->n = n;
+	new-> = n;
 
 	if (idx == 0)
 	{
