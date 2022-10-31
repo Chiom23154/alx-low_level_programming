9e523cdda557b16f0ddc7a3c10dc8f3e10b6c6b4
Uduaka Colette Chioma <Opriscy42@gmail.com>
2022-10-31 10:24:12 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: filename.
- * @text_content: added content.
+ * @text_content:added content.
  *
  * Return: 1 if the file exists. -1 if the fails does not exist
  * or if it fails.
@@ -35,5 +35,5 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	close(fd);
 
-	return (1):
-}	
+	return (1);
+}
