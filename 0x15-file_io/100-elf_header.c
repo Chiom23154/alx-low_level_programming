392701bc31b638bdaf2038b22d69fc7f104e8473
Uduaka Colette Chioma <Opriscy42@gmail.com>
2022-11-01 06:27:39 -0700

    task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -74,22 +74,22 @@ void print_magic(unsigned char *e_ident)
  */
 void print_class(unsigned char *e_ident)
 {
-       printf("  Class:                             ");
+	printf("  Class:                             ");
 
-       switch (e_ident[EI_CLASS])       
-       {
-       case ELFCLASSNONE:
-	       printf("none\n");
-	       break;
-       case ELFCLASS32:
-	       printf("ELF32\n");
-	       break;
-       case ELFCLASS64:
-	       printf("ELF64\n");
-	       break;
-       default:
-	       printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-       }
+	switch (e_ident[EI_CLASS])
+	{
+	case ELFCLASSNONE:
+		printf("none\n");
+		break;
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
 }
 
 /**
@@ -316,5 +316,4 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	free(header);
 	close_elf(o);
 	return (0);
-}
-       
+}       
